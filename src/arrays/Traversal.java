58acd7bf3f82a3e99ed6452a9bdf884fc5a7ef7a
MrPrasad08 <durgaprasadalb@gmail.com>
2026-08-11 19:48:39 +0530

    Basic Array Traversal

---
+++ src/arrays/Traversal.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class Traversal {
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 40, 50 };
+		for (int n : arr) {
+			System.out.println(n);
+		}
+	}
+}
