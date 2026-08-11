9726e20cd785e02a6bff64c9378628d0988b3548
MrPrasad08 <durgaprasadalb@gmail.com>
2026-08-11 20:02:38 +0530

    Basic array traversal, insertion, deletion, updating.....

--- src/arrays/Traversal.java
+++ src/arrays/Traversal.java
@@ -2,9 +2,39 @@ package arrays;
 
 public class Traversal {
 	public static void main(String[] args) {
-		int[] arr = { 10, 20, 30, 40, 50 };
+		int[] arr = new int[6];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		// Traverse Each Element
+		System.out.println("Traversing............");
 		for (int n : arr) {
-			System.out.println(n);
+			System.out.print(n + " ");
+		}
+		// Insert the element at particular position
+		int pos = 2;
+		for (int i = arr.length - 1; i > pos; i--) {
+			arr[i] = arr[i - 1];
+		}
+		arr[pos] = 25;
+		System.out.println("\n\nInserting............");
+		for (int n : arr) {
+			System.out.print(n + " ");
+		}
+
+		// Deleting the element from the array
+		int del = 3;
+		for (int i = del; i < arr.length-1; i++) {
+			arr[i] = arr[i + 1];
+		}
+		int n = arr.length;
+		n--;
+		System.out.println("\n\nDeleting...............");
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
 		}
 	}
 }
