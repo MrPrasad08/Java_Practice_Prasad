23d355c5602fb332128101846eee009f2393ac74
MrPrasad08 <durgaprasadalb@gmail.com>
2026-08-14 18:11:05 +0530

    Update the array for target sum

--- src/patternsDSA/TwoPointersAsc.java
+++ src/patternsDSA/TwoPointersAsc.java
@@ -1,7 +1,32 @@
 package patternsDSA;
 
 public class TwoPointersAsc {
-public static void main(String[] args) {
-	
-}
+	static void TargetSum(int[] arr) {
+		System.out.println("Array : ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println("\n\nValues for target Sum : ");
+		int target = 11;
+		int left = 0;
+		int right = arr.length - 1;
+		while (left < right) {
+			int sum = arr[left] + arr[right];
+
+			if (sum == target) {
+				System.out.println(arr[left] + " + " + arr[right] + " = " + target);
+				left++;
+				right--;
+			} else if (sum > target) {
+				right--;
+			} else {
+				left++;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 4, 6, 7, 8, 9, 13 };
+		TargetSum(arr);
+	}
 }
