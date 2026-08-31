f07cc0b1ae78f8ef7fa9c6e71223b152b99666fc
MrPrasad08 <durgaprasadalb@gmail.com>
2026-08-31 10:03:02 +0530

    Basic methods

---
+++ src/examples/TestDemoMethods1.java
@@ -0,0 +1,16 @@
+package examples;
+
+public class TestDemoMethods1 {
+	void method1() {
+		System.out.println("One");
+	}
+	void method2() {
+		System.out.println("Two");
+	}
+	void method3() {
+		System.out.println("Three");
+	}
+	void method4() {
+		System.out.println("Four");
+	}
+}
