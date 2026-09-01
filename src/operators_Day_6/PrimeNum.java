8e75c23f8614563e7078c8ed7e28adf129dca84f
MrPrasad08 <durgaprasadalb@gmail.com>
2026-09-01 14:03:36 +0530

    Prime numbers between two numbers.

--- src/operators_Day_6/PrimeNum.java
+++ src/operators_Day_6/PrimeNum.java
@@ -1,9 +1,13 @@
 package operators_Day_6;
 
-public class PrimeNum {
-	void prime(int num) {
+import java.util.Scanner;
 
-		for (int i = 1; i <= num; i++) {
+public class PrimeNum {
+	void primes(int start, int end) {
+		System.out.println("Start : "+start);
+		System.out.println("Ending : "+end);
+		System.out.println("Prime numbers between "+start+" and "+end+" are : ");
+		for (int i = start; i <= end; i++) {
 			int count = 0;
 			for (int j = 1; j <= Math.sqrt(i); j++) {
 				if (i % j == 0) {
@@ -14,13 +18,16 @@ public class PrimeNum {
 				System.out.print(i + " ");
 			}
 		}
-
 	}
 
 	public static void main(String[] args) {
-		System.out.println("main method started !!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter starting number and end number : ");
+		int start = sc.nextInt();
+		int end = sc.nextInt();
 		PrimeNum obj = new PrimeNum();
-		obj.prime(100);
+		obj.primes(start, end);
+
 	}
 }
 // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
