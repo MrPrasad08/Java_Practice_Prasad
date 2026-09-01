d1477a9eced14dcf784a2fa6440109c805f7dd15
MrPrasad08 <durgaprasadalb@gmail.com>
2026-09-01 14:24:44 +0530

    Basic operations perform between two operands......

---
+++ src/examples/TestDemoOne.java
@@ -0,0 +1,25 @@
+package examples;
+
+public class TestDemoOne {
+	void results(int a, int b) {
+		addition(a, b);
+		subtraction(a, b);
+	}
+
+	void addition(int a, int b) {
+		System.out.println("Adddition : " + (a + b));
+	}
+
+	void subtraction(int a, int b) {
+		System.out.println("Subtraction is : " + (a - b));
+	}
+
+	void multiplication(int a, int b) {
+
+	}
+
+	public static void main(String[] args) {
+		TestDemoOne obj = new TestDemoOne();
+		obj.results(2, 4);
+	}
+}
---
+++ src/operators_Day_6/TestDemo1.java
@@ -0,0 +1,12 @@
+package operators_Day_6;
+
+import java.lang.Math;
+
+public class TestDemo1 {
+	public static void main(String[] args) {
+		int radius = 20;
+		double result = Math.PI * radius * radius;
+		
+		System.out.println(Math.abs(result));
+	}
+}
