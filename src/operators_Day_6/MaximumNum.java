2dfe33b6c78f4039b9f05331a896fc4f315d0a50
MrPrasad08 <durgaprasadalb@gmail.com>
2026-09-06 11:21:08 +0530

    Maximum digit in a number

---
+++ src/operators_Day_6/MaximumNum.java
@@ -0,0 +1,11 @@
+package operators_Day_6;
+
+public class MaximumNum {
+	public static void main(String[] args) {
+		int a = 90;
+		int b = 30;
+		
+		String result = a>b ? "a is big" : "b is big";
+		System.out.println(result);
+	}
+}
