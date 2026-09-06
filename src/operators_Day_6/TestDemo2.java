7abadff3413719e9b6cb0f92d7a8a642ea830e1d
MrPrasad08 <durgaprasadalb@gmail.com>
2026-09-06 11:19:57 +0530

    Basic

---
+++ src/operators_Day_6/TestDemo2.java
@@ -0,0 +1,20 @@
+package operators_Day_6;
+
+public class TestDemo2 {
+	public static void main(String[] args) {
+//		double x = 10;
+//		int y = 3;
+//		System.out.println(x/y);
+		
+		int x = 10;
+		int y = 3;
+		double z = x / y;
+		System.out.println(z);
+		
+		
+		
+//		int x = 10;
+//		System.out.println(x < 20 || x++ > 30);
+//		System.out.println(x);
+	}
+}
